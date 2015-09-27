3cce0b43910f7aa0f5378482e48405aa5421a282
Guscha Anna <ann9510.as@gmail.com>
2015-09-27 21:33:47 +0300

    the second version

---
+++ Buy.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_ForTraining
+{
+    internal enum Discount
+    {
+        noDiscount,
+        Type1,
+        Type2,
+        Type3
+    }
+
+    internal class Buy: IComparable<Buy>
+    {
+        private int day;       
+        private int count;
+
+        private static int month;
+        private static int price;
+
+        private Discount discount;
+
+        private static int transportationСosts;
+
+        public static int Month
+        {
+            get { return month; }
+            set { month = value; }
+        }
+
+        public static int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public static int TransportationСosts
+        {
+            get { return transportationСosts; }
+            set { transportationСosts = value; }
+        }
+
+        private int percent;
+        private int costBonusItem;
+
+        public int Day
+        {
+            get { return day; }
+            set { day = value; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public Discount Discount
+        {
+            get { return discount; }
+            set { discount = value; }
+        }
+
+        public Buy(int day, int count)
+        {
+            this.day = day;
+            this.count = count;
+            this.discount = Discount.noDiscount;
+        }
+
+        public Buy(int day, int count, Discount discountType)
+        {
+            this.day = day;
+            this.count = count;          
+            if (discountType == Discount.Type2)
+            {
+                this.discount = discountType;
+            }
+        }
+
+        public Buy (int day,  int count, Discount discountType, int discount )
+        {
+            this.day = day;
+            this.count = count;
+            if ( discountType == Discount.Type1 )
+            {
+                this.percent = discount;
+            }
+            else if ( discountType == Discount.Type3 )
+            {
+                this.costBonusItem = discount;
+            }
+            this.discount = discountType;
+        }
+
+        public int Cost()
+        {
+            int cost=0;
+            cost = price*count;
+            switch (discount)
+            {
+                case Discount.Type1:
+                {
+                    cost = cost*(1 - percent/100);
+                }
+                    break;
+                case Discount.Type2:
+                {
+                    cost = cost - transportationСosts;                     
+                }
+                    break;
+                case Discount.Type3:
+                {
+                    cost = cost - costBonusItem;
+                }
+                    break;
+            }
+            return cost;
+        }
+
+        public int CompareTo ( Buy obj )
+        {
+            if ( obj.Day<this.Day)
+                return 1;
+            else if (obj.Day>this.Day)
+            {
+                return -1;
+            }
+            else
+                return 0;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("День: {0}, Кол-во: {1}, Скидка: {2}, Общая ст-ть: {3}", Day, Count, Discount, Cost());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_ForTraining
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            List<Buy> items = new List<Buy>();
+
+            Buy.TransportationСosts = 20;
+            Buy.Price = 300;
+            Buy.Month = 3;
+
+            items.Add(new Buy(5,3,Discount.Type1, 20));
+            items.Add ( new Buy ( 5, 2, Discount.Type1, 10 ) );
+            items.Add ( new Buy ( 7, 3,  Discount.Type2) );
+            items.Add ( new Buy ( 25, 5, Discount.Type1, 5 ) );
+            items.Add ( new Buy ( 1,  3, Discount.Type3, 300) );
+            items.Add ( new Buy ( 10,  2, Discount.Type2) );
+            items.Add ( new Buy ( 3,  4, Discount.Type1, 20 ) );
+            items.Add ( new Buy (11,  6,  Discount.Type1, 5 ) );
+            items.Add ( new Buy (  27,  2,  Discount.Type3, 300 ) );
+            items.Add ( new Buy ( 1,  3,  Discount.Type3, 200 ) );
+            items.Add ( new Buy ( 10,  2,  Discount.Type2 ) );
+            items.Add ( new Buy ( 22,  4, Discount.Type1, 15 ) );
+
+            items.Sort();
+
+            Console.WriteLine("Цена 1 товара: " + Buy.Price + " бел. руб.");
+            Console.WriteLine ( "Текущий месяц: " + Buy.Month);
+
+            int countBuy=0;
+            foreach (var item in items)
+            {
+                if (item.Day == 10)
+                    countBuy++;
+                Console.WriteLine(item.ToString());
+            }
+
+            if (countBuy > 0)
+            {
+                Console.WriteLine("Покупок 10-го числа: " + countBuy);
+            }
+            else
+            {
+                Console.WriteLine ( "10-го числа не совершено ни одной покупки." );
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
